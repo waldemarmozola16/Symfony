0f3c534afcf54d164f5ad434b0571d3ac381755c
Waldemar Mozola <waldemar.mozola@onet.pl>
2019-01-15 00:43:43 +0100

    internal error

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -16,11 +16,11 @@ use Symfony\Component\HttpFoundation\Response;
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
     public function homepage()
     {
-        return new Response('OMG! My first page already! Wooo!');
+        return $this->render('article/homepage.html.twig');
     }
 
     /**
